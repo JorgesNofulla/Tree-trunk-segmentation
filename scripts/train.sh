12bb10020f8cd0cb781ba3a78cf882a26bb331b3
JorgesNofulla <113198223+JorgesNofulla@users.noreply.github.com>
2023-02-26 11:37:36 +0100

    Delete train.sh

--- scripts/train.sh
+++
@@ -1,8 +0,0 @@
-# Set some environment stuffs
-export TRAINING_MODE='simple'
-
-# Make it clear what is happening
-echo "Starting training..."
-
-# Do something (somewhat) useful
-python src/train.py 
